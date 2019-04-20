d629413f516ef35abf4ecd027b4e91a12a06f40c
mohitghodake <mohitghodake@live.com>
2019-04-20 01:01:08 -0400

    minor changes

--- Assignment7.R
+++ Assignment7.R
@@ -18,6 +18,8 @@ library(e1071)
 library(tree)
 library(ISLR)
 library(data.table)
+library(arules)
+library(arulesViz)
 
 df <- fread("Credit.csv")
 
@@ -57,13 +59,19 @@ df$TYPE4 <- factor(ifelse(df$TYPE == 4, 1, 0))
 df$TYPE5 <- factor(ifelse(df$TYPE == 5, 1, 0))
 df$TYPE6 <- factor(ifelse(df$TYPE == 6, 1, 0))
 
+df$CHK_ACCT <- NULL
+df$SAV_ACCT <- NULL
+df$HISTORY <- NULL
+df$JOB <- NULL
+df$TYPE <- NULL
+
 ##
 #2
 set.seed(12345)
 df$PROFITABLE <- ifelse(df$NPV > 0, 1, 0)
 df$PROFITABLE <- factor(df$PROFITABLE)
-df1 <- df[, -21]
-df1 <- df1[, -46]
+df1 <- df[, -16]
+df1 <- df1[, -41]
 df1 <- scale(df1)
 
 kmeansCluster = kmeans(df1, 5, nstart=20)
@@ -86,5 +94,58 @@ dist(kmeansCluster_4$centers)
 kmeansCluster_6 = kmeans(df1, 9, nstart=20)
 kmeansCluster_6
 dist(kmeansCluster_6$centers)
+
+
+##
+#7
+df <- fread("Credit.csv")
+df$AMOUNT_REQUESTED <- as.numeric(gsub(",","",df$AMOUNT_REQUESTED))
+df$CREDIT_EXTENDED <- NULL
+df$`OBS#` <- NULL
+
+#Creating Dummy Varaibles
+df$CHK0 <- factor(ifelse(df$CHK_ACCT == 0, 1, 0))
+df$CHK1 <- factor(ifelse(df$CHK_ACCT == 1, 1, 0))
+df$CHK2 <- factor(ifelse(df$CHK_ACCT == 2, 1, 0))
+df$CHK3 <- factor(ifelse(df$CHK_ACCT == 3, 1, 0))
+
+df$SAV0 <- factor(ifelse(df$SAV_ACCT == 0, 1, 0))
+df$SAV1 <- factor(ifelse(df$SAV_ACCT == 1, 1, 0))
+df$SAV2 <- factor(ifelse(df$SAV_ACCT == 2, 1, 0))
+df$SAV3 <- factor(ifelse(df$SAV_ACCT == 3, 1, 0))
+df$SAV4 <- factor(ifelse(df$SAV_ACCT == 4, 1, 0))
+
+df$HISTORY0 <- factor(ifelse(df$HISTORY == 0, 1, 0))
+df$HISTORY1 <- factor(ifelse(df$HISTORY == 1, 1, 0))
+df$HISTORY2 <- factor(ifelse(df$HISTORY == 2, 1, 0))
+df$HISTORY3 <- factor(ifelse(df$HISTORY == 3, 1, 0))
+df$HISTORY4 <- factor(ifelse(df$HISTORY == 4, 1, 0))
+
+df$JOB0 <- factor(ifelse(df$JOB == 0, 1, 0))
+df$JOB1 <- factor(ifelse(df$JOB == 1, 1, 0))
+df$JOB2 <- factor(ifelse(df$JOB == 2, 1, 0))
+df$JOB3 <- factor(ifelse(df$JOB == 3, 1, 0))
+
+df$TYPE0 <- factor(ifelse(df$TYPE == 0, 1, 0))
+df$TYPE1 <- factor(ifelse(df$TYPE == 1, 1, 0))
+df$TYPE2 <- factor(ifelse(df$TYPE == 2, 1, 0))
+df$TYPE3 <- factor(ifelse(df$TYPE == 3, 1, 0))
+df$TYPE4 <- factor(ifelse(df$TYPE == 4, 1, 0))
+df$TYPE5 <- factor(ifelse(df$TYPE == 5, 1, 0))
+df$TYPE6 <- factor(ifelse(df$TYPE == 6, 1, 0))
+
+df$CHK_ACCT <- NULL
+df$SAV_ACCT <- NULL
+df$HISTORY <- NULL
+df$JOB <- NULL
+df$TYPE <- NULL
+df <- df[, -(1:16)]
+
+set.seed(12345)
+
+# Visualize the items inside the data
+itemFrequencyPlot(df, topN = 15, type = "relative")
+
+rules <- apriori(df, parameter = list(supp = 0.1, conf = 0.8))
 
 
